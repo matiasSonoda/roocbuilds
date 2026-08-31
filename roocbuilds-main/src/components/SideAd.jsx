3684fa0881e18a4eb5ada17213761a422e1410c2
hernan <hernan.maiorana.96@gmail.com>
2026-08-31 10:45:36 -0300

    Agrego anuncios, ajustes de responsivodad y botones de votacion

---
+++ roocbuilds-main/src/components/SideAd.jsx
@@ -0,0 +1,10 @@
+import styles from './SideAd.module.css';
+
+export function SideAd() {
+    return (
+    <div className={styles.sideAdContainer}>
+        <span className={styles.adText}>Anuncio lateral</span>
+    </div>
+    );
+}
+/* Esto es para los anuncios fijos a los costados de las páginas */
